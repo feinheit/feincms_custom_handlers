d136da3574982499aefc6b52e077c7b6c8430068
Simon Bächler <sb@feinheit.ch>
2011-11-29 18:17:56 +0100

    add master handler class, legacy module

---
+++ feincms_handlers/legacy/__init__.py
@@ -0,0 +1,52 @@
+""" This module is for FeinCMS < 1.5
+"""
+
+from django.http import Http404
+from feincms.utils import get_object
+
+class MasterHandler(object):
+    """ This is where you register your handlers. They will be called one after the
+        other until one returns a response.
+    """
+
+    def __init__(self, handlers=None, *args, **kwargs):
+        self.handlers = []
+        if handlers:
+            self.register(handlers)
+
+    def _register_handler(self, handler):
+        handler = get_object(handler) # parse strings
+        if not hasattr(handler, '__call__'):
+            raise AttributeError('Handler %s has no method "__call__". Needs one.' % handler)
+        self.handlers.append(handler)
+
+    def register(self, handlers):
+        if isinstance(handlers, list):
+            for h in handlers:
+                self._register_handler(h)
+        else:
+            self._register_handler(handlers)
+
+    def unregister(self, handler):
+        self.handlers.remove(handler)
+
+
+    def __call__(self, request, path=None):
+        for handler in self.handlers:
+            try:
+                return handler.__call__(request, path)
+            except Http404:
+                continue
+
+        # No response until now:
+        raise Http404
+
+    @property
+    def __name__(self):
+        """
+        Dummy property to make this handler behave like a normal function.
+        This property is used by django-debug-toolbar
+        """
+        return self.__class__.__name__
+
+handler = MasterHandler()
---
+++ feincms_handlers/legacy/page_id_fallback.py
@@ -0,0 +1,21 @@
+from feincms.views.base import Handler
+from django.shortcuts import get_object_or_404, redirect
+from django.http import Http404
+from feincms.module.page.models import Page
+
+class PageIdFallbackHandler(Handler):
+    """ This Handler is used if you have static links within your content.
+        You can append a get parameter 'p' with the page id. The CMS will
+        find the page even if it has been moved.
+    """
+
+    def __call__(self, request, path=None):
+        if not request.GET.get('p', None):
+            raise Http404
+        try:
+            page = get_object_or_404(Page, pk=request.GET.get('p', None))
+        except ValueError:
+            raise Http404
+        return redirect(page)
+
+handler = PageIdFallbackHandler()
