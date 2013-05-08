f46637f7eb4d3d95e7656717da5801fa5dd19a22
Simon Bächler <sb@feinheit.ch>
2013-05-08 10:09:49 +0200

    add setup.py and stuff

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='feincms_custom_handlers',
+    version='0.2',
+    packages=['feincms_handlers', 'feincms_handlers.views',
+              'feincms_handlers.views.cbv'],
+    url='https://github.com/feinheit/feincms_custom_handlers',
+    license='BSD License',
+    author='Simon Baechler',
+    author_email='sb@feinheit.ch',
+    description='This module allows to include custom feincms handlers. Major use cases are ajax-responses that only return a small part of the page or mobile versions of the site.'
+)
