79dd2381ef08a05c0cf34cb63af026be94af503f
Simon Bächler <sb@feinheit.ch>
2013-07-04 12:34:13 +0200

    add page to request

--- feincms_handlers/views/cbv/htmlsnapshot.py
+++ feincms_handlers/views/cbv/htmlsnapshot.py
@@ -1,6 +1,7 @@
 #coding=utf-8
 from feincms_handlers import NotMyJob
 from django.template import Template
+from django.utils.translation import get_language
 import urllib
 
 from feincms.views.cbv.views import Handler
@@ -9,12 +10,30 @@ from feincms.module.page.models import Page
 class HtmlSnapshotHandler(Handler):
     """
     Handler for Google's _escaped_fragment_ crawl request
+    Requires the slug to be unique for each language.
+
     """
     def dispatch(self, request, *args, **kwargs):
         escaped_fragment = request.GET.get('_escaped_fragment_', None)
         if not escaped_fragment:
             raise NotMyJob('escapedfragment')
-        setattr(request, 'escaped_fragment', urllib.unquote(escaped_fragment))
+        escaped_fragment = urllib.unquote(escaped_fragment)
+        setattr(request, 'escaped_fragment', escaped_fragment)
+        page_model = self.page_model
+        pages = page_model.objects.filter(slug=escaped_fragment)
+        page = None
+        if len(pages) == 1:
+            page = pages[0]
+        elif len(pages) > 1 and hasattr(page_model, 'language'):
+            current_lang = get_language()
+            for p in pages:
+                if p.language == current_lang:
+                    page = p
+                    break
+            if page == None:
+                page = pages[0]
+
+        setattr(request, 'snapshot_page', page)
         return super(HtmlSnapshotHandler, self).dispatch(request, *args, **kwargs)
 
 
